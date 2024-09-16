aff8751a8ea450a75b205e3fe877bada0cb7876e
UznetDev <uznattv@gmail.com>
2024-09-16 13:08:51 +0500

    update

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
 from db.mysql_db import Database
-from data.config import *  # Import all configurations from the config module
+from data.config import *
 
 
 db = Database(host=HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DATABASE)
