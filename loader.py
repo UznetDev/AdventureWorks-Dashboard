b41a1b5b6f79401bf4f43a8fadb6bd3d4718a566
Abdurahmon Niyozaliev <121486055+UznetDev@users.noreply.github.com>
2024-09-18 18:33:24 +0500

    Update loader.py (cleaning) # 1

--- loader.py
+++ loader.py
@@ -1,5 +1,8 @@
 from db.mysql_db import Database
-from data.config import *
+from data.config import HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
 
 
-db = Database(host=HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DATABASE)
+db = Database(host=HOST, 
+              user=MYSQL_USER, 
+              password=MYSQL_PASSWORD, 
+              database=MYSQL_DATABASE)
