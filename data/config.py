f81c4a149431fde21757a9341bc9f44c04ce7693
Abdurahmon Niyozaliev <121486055+UznetDev@users.noreply.github.com>
2024-09-18 18:35:11 +0500

    Update config.py (add instruction) #1

--- data/config.py
+++ data/config.py
@@ -6,7 +6,10 @@ from environs import Env
 env = Env()
 env.read_env()
 
-HOST = env.str('HOST')  # The host for the database
+HOST = env.str('HOST')  # The host for the database (default localhost)
+
 MYSQL_USER = env.str('MYSQL_USER')  # The MySQL database user
+
 MYSQL_PASSWORD = env.str('MYSQL_PASSWORD')  # The MySQL database password
+
 MYSQL_DATABASE = env.str('MYSQL_DATABASE')  # The MySQL database name
